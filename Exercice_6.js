3d0cc8cf9259d154ad73878f3af35b2db812d387
William Touboul <Touboul.william@gmail.com>
2021-01-04 13:12:35 +0100

    ajout css

--- Exercice_6.js
+++ Exercice_6.js
@@ -3,11 +3,23 @@ document.getElementById("submit").addEventListener('click',function math(e) {
 
     e.preventDefault(); // Sert a empecher le refresh
 
-    let firstNumber = document.getElementById("firstNumber").value;
-    let secondNumber = document.getElementById("secondNumber").value;
+    let firstNumber = Number(document.getElementById("firstNumber").value);
+    let secondNumber = Number(document.getElementById("secondNumber").value);
+    
+    if ( isNaN(firstNumber) ) {
+        document.getElementById("errorFirstNumber").innerHTML ="Erreur dans la saisie du premier nombre";
+    } else if (isNaN(secondNumber)) {
+        document.getElementById("errorSecondNumber").innerHTML="Erreur dans la saisie du second nombre";
+    }
+    else {
+        let result = firstNumber % secondNumber;
+        document.getElementById("errorFirstNumber").innerHTML="";
+        document.getElementById("errorSecondNumber").innerHTML="";        
+        document.getElementById("resultBox").innerHTML = "Le reste de la division des deux nombres est " + result;
+    }
 
-    let result = firstNumber % secondNumber;
-    document.getElementById("resultBox").innerHTML = result;
+
+
 
 })
 
