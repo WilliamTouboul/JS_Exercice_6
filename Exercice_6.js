c50d1c31af957abb3378e817af51ba87982e77d0
William Touboul <Touboul.william@gmail.com>
2021-01-04 11:42:08 +0100

    correction, utilisation d'un addEventListner

--- Exercice_6.js
+++ Exercice_6.js
@@ -1,9 +1,13 @@
-function math() {
+
+document.getElementById("submit").addEventListener('click',function math(e) {
+
+    e.preventDefault(); // Sert a empecher le refresh
 
     let firstNumber = document.getElementById("firstNumber").value;
     let secondNumber = document.getElementById("secondNumber").value;
 
     let result = firstNumber % secondNumber;
     document.getElementById("resultBox").innerHTML = result;
-}
+
+})
 
